90af4e4bbd0a8ae5458b12025b5fcc7d9d0b7ec6
Austin Southward <jsaw0618@gmail.com>
2025-02-21 16:22:56 -0500

    Adjust script

--- vim-config/vimscript.sh
+++ vim-config/vimscript.sh
@@ -1,6 +1,11 @@
 echo "Copying over VIM configuration..."
-if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -f ~/.vim/bundle/Vundle.vim/README.md ]; then
+        echo "Vundle not found. Installing from https://github.com/VundleVim/Vundle.vim.git"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	cp ~/dotfiles/vim-config/.vimrc ~/.vimrc
+        vim +PluginInstall +qall
+else
+	echo "Vundle found. Copying .vimrc from repo to home directory..."
+	cp ~/dotfiles/vim-config/.vimrc ~/.vimrc
+        vim +PluginInstall +qall
 fi
-cp ~/dotfiles/vim-config/.vimrc ~/.vimrc
-vim +PluginInstall +qall
